3b44eedda6f3880473c4c02304fd68bcea9f06b9
RicoBSJ <aubrun.eric@free.fr>
2019-04-22 03:14:26 +0200

    1 Commit Home 22_04

---
+++ src/Essai.java
@@ -0,0 +1,12 @@
+
+class Essai {
+	public static void main(String[] args) {
+		for (int loop1 = 1; loop1 <= 4; loop1 = loop1 + 1) {
+
+			for (int loop2 = 1; loop2 <= 2; loop2 = loop2 + 1) {
+				System.out.print("OX");
+			}
+			System.out.println();
+		}
+	}
+}
